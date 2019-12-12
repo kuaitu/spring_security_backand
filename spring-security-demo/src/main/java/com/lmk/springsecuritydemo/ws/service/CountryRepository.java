645d74d53e4b2e148be8cd543d2691e754001bed
kuaitu <657781930@qq.com>
2019-12-12 14:58:40 +0800

    1、去掉wsdl4j

--- spring-security-demo/src/main/java/com/lmk/springsecuritydemo/ws/service/CountryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lmk.springsecuritydemo.ws.service;
-
-import com.lmk.springsecuritydemo.ws.model.Country;
-
-public interface CountryRepository {
-    Country findCountry(String name);
-}
